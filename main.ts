62145ac24560e03834c3f2512addca9be90e74a7
IkrlegidO <73440160+Ikrlegido12@users.noreply.github.com>
2022-11-11 17:42:19 +0100

    Ultimate

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(180)
+    basic.showIcon(IconNames.Happy)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.InBackground)
+    servos.P0.setAngle(90)
+    basic.pause(2000)
     basic.showLeds(`
         . . . . .
         # # . # #
@@ -8,6 +13,11 @@ input.onButtonPressed(Button.A, function () {
         `)
 })
 input.onPinPressed(TouchPin.P1, function () {
+    servos.P0.setAngle(180)
+    basic.showIcon(IconNames.Happy)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    servos.P0.setAngle(90)
+    basic.pause(2000)
     basic.showLeds(`
         . . . . .
         # # . # #
@@ -16,6 +26,8 @@ input.onPinPressed(TouchPin.P1, function () {
         # . . . #
         `)
 })
+music.setBuiltInSpeakerEnabled(true)
+servos.P0.setAngle(90)
 basic.showLeds(`
     . . . . .
     # # . # #
@@ -23,3 +35,10 @@ basic.showLeds(`
     . # # # .
     # . . . #
     `)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+// Hecho por IkrlegidO
+// ===================
+// Chatarra Colector
+// Moneditas
+// ===================
+// I.E.S. Bahía de Algeciras
